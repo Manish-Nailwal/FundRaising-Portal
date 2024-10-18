32cf5024af961494b421114a3d3bb5d07f47d9e8
Manish-Nailwal <110821711+Manish-Nailwal@users.noreply.github.com>
2024-10-18 23:35:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 3000, // Use Render's PORT
     host: '0.0.0.0', // Allow access from outside
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Your backend's local address
+        changeOrigin: true,
+      },
   },
 })
